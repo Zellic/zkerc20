f4d5a3d219ff114f0e3694f1ce8b9f70e87c4b12
Aaron Esau <git@aaronesau.com>
2024-09-21 23:14:41 +0800

    progress on deploy script

--- scripts/Deploy.s.sol
+++ scripts/Deploy.s.sol
@@ -2,30 +2,77 @@ pragma solidity ^0.8.27;
 
 import { Node } from "../contracts/Node.sol";
 import { LZBridge } from "../contracts/bridges/LZBridge.sol";
-import { MockERC20 } from "forge-std/MockERC20.sol";
+import { CCIPBridge } from "../contracts/bridges/CCIPBridge.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Deploy {
-    emit MockERC20Deployed(address token);
-    emit Deployed(address node, address lzBridge);
 
-    constructor(address _lzEndpoint) {
+contract GM is ERC20 {
+    constructor(address sender) ERC20("GM", "GM") {
+        _mint(sender, 1000000 * 10 ** decimals());
+    }
+}
+
+
+contract Deploy is Ownable {
+    event Deployed(address node, address lzBridge, address ccipBridge);
+
+    Node node public;
+    LZBridge lzBridge public;
+    CCIPBridge ccipBridge public;
+
+    // DO NOT CHANGE. ONLY ADD NEW BRIDGE TYPES
+    uint8 public constant LZ_BRIDGE = 10;
+    uint8 public constant CCIP_BRIDGE = 20;
+
+    constructor(address _lzEndpoint, address _ccipRouter) {
         address deployer = msg.sender;
+        transferOwnership(deployer);
 
-        Node node = new Node();
+        node = new Node();
+        lzBridge = new LZBridge(deployer, address(node), _lzEndpoint);
+        ccipBridge = new CCIPBridge(deployer, address(node), _ccipRouter);
+        event Deployed(address(node), address(lzBridge), address(ccipBridge));
 
-        LZBridge lzBridge = new LZBridge(deployer, address(node), _lzEndpoint);
-        node.configureBridge(1, address(lzBridge));
-
-        // remove owner
-        node.renounceOwnership();
-        event Deployed(address(node), address(lzBridge));
-
-        mintDemoERC20();
+        // configure node to accept LZ and CCIP bridges
+        node.configureBridge(LZ_BRIDGE, address(lzBridge));
+        node.configureBridge(CCIP_BRIDGE, address(ccipBridge));
     }
 
 
-    function mintDemoERC20() {
-        MockERC20 token = new MockERC20("Demo", "DEMO", 18, 1000000000000000000000000000);
-        emit MockERC20Deployed(address(token));
+    function connectLz(uint256 destChainId, uint32 eid, address counterparty) public onlyOwner {
+        lzBridge.configureChainId(destChainId, eid);
+        // NOTE: we are leaving the default LZ config as is
+        lzBridge.setPeer(eid, bytes32(bytes20(counterparty)));
+    }
+
+
+    function connectCCIP(uint256 destChainId, uint64 selector, address counterparty) public onlyOwner {
+        ccipBridge.configureChainId(destChainId, selector, counterparty);
+    }
+
+
+    function demo() public onlyOwner returns (address memecoin) {
+        // create new memecoin and mint to sender
+        memecoin = _createMemecoin();
+    }
+
+
+    // create new memecoin and mint to sender
+    function _createMemecoin() internal returns (address) {
+        GM gm = new GM(msg.sender);
+        return address(gm);
+    }
+
+
+    function renounce() public onlyOwner {
+        // remove ability for this contract to configure Node/BridgeManager
+        // NOTE: commented out because we want ability to add bridges later
+        //node.renounceOwnership();
+
+        // remove ability to call connect function on this contract.
+        renounceOwnership();
+
+        // LZBridge will retain the deployer as owner
+        // TODO: should transfer ownership to multisig/governance later
     }
 }
---
+++ scripts/deploy.ts
@@ -0,0 +1,129 @@
+// deploy the Deploy.s.sol contract on the two networks
+
+import { ethers } from "hardhat";
+require('dotenv').config()
+
+
+async function deployAndSetup() {
+    let lzEndpoint = '0x1234567890123456789012345678901234567890'; // TODO
+    let ccipRouter = '0x1234567890123456789012345678901234567890'; // TODO
+
+    // array of providers, where provider has the network name and chain id
+    let providers = [
+        {
+            name: 'goerli',
+            chainId: 10,
+            lzChainId: 1, // TODO
+            ccipChainId: 1 // TODO
+        },
+        {
+            name: 'rinkeby',
+            chainId: 20,
+            lzChainId: 1, // TODO
+            ccipChainId: 1 // TODO
+        }
+    ];
+
+
+    //let privateKey = '0x0123456789012345678901234567890123456789012345678901234567890123'; // TODO
+    let privateKey = process.env.PRIVATE_KEY;
+
+
+    //////////
+
+
+    // 1. Deploy all contracts
+    let numDeployed = 0;
+    await Promise.all(providers.map(async (p) => {
+        // Initialize provider and wallet
+        p.provider = ethers.getDefaultProvider(p.name);
+        p.wallet = new ethers.Wallet(privateKey, p.provider);
+
+        console.log("Deploying contract on", p.name, "with the account:", p.wallet.address);
+
+        // Deploy the contract
+        const Deploy = await ethers.getContractFactory("Deploy", p.wallet);
+        p.deploy = await Deploy.deploy(lzEndpoint, ccipRouter);
+        console.log("... deploying to address:", p.deploy.address);
+        console.log("... transaction hash:", p.deploy.deployTransaction.hash);
+
+        // Wait for the deployment to be mined
+        await p.deploy.deployTransaction.wait();
+        await p.deploy.deployed();
+        numDeployed++;
+        console.log(`... deployment on ${p.name} was mined (${providers.length - numDeployed} remaining)`);
+    }));
+
+    // 2. Connect all deployments to each other
+    console.log(`Connecting deployments to each other (${providers.length}x${providers.length} matrix)...`);
+    for (let i = 0; i < providers.length; i++) {
+        let p = providers[i];
+
+        // 3.a. Get the LZBridge and CCIPBridge addresses
+        console.log("... getting LZBridge and CCIPBridge addresses for", p.name);
+        p.lzAddr = await p.deploy.lzBridge();
+        p.ccipAddr = await p.deploy.ccipBridge();
+        console.log("\tLZBridge:", p.lzAddr);
+        console.log("\tCCIPBridge:", p.ccipAddr);
+
+        // 3.b. Connect deployments
+        for (let j = 0; j < providers.length; j++) {
+            if (i == j) continue; // skip self-connection
+            let q = providers[j];
+            
+            // connectLz(uint256 destChainId, uint32 eid, address counterparty)
+            console.log(`... connecting ${p.name} to ${q.name} on LayerZero...`);
+            await p.deploy.connectLz(q.chainId, q.lzChainId, p.lzAddr);
+
+            // connectCCIP(uint256 destChainId, uint64 selector, address counterparty)
+            console.log(`... connecting ${p.name} to ${q.name} on CCIP...`);
+            await p.deploy.connectCCIP(q.chainId, q.ccipChainId, p.ccipAddr);
+        }
+    }
+
+
+    // 3. On one chain, create a memecoin
+    {
+        let p = providers[0];
+        console.log("Creating a memecoin on", p.name, "...");
+
+        // returns memecoin address
+        p.memecoin = await p.deploy.demo();
+        console.log("... memecoin address:", p.memecoin);
+    }
+
+
+    // 4. Renounce ownership
+    console.log("Renouncing ownership of all deployments...");
+    for (let i = 0; i < providers.length; i++) {
+        let p = providers[i];
+        await p.deploy.renounceOwnership();
+    }
+
+
+    // 5. Create a JSON file with the deployments
+    let fs = require('fs');
+    let deployments = {};
+    for (let i = 0; i < providers.length; i++) {
+        let p = providers[i];
+        deployments[p.name] = {
+            address: p.deploy.address,
+            deployer: p.wallet.address,
+            memecoin: p.memecoin || null,
+
+            chainId: p.chainId,
+            lzChainId: p.lzChainId,
+            ccipChainId: p.ccipChainId,
+            lzAddr: p.lzAddr,
+            ccipAddr: p.ccipAddr,
+        };
+    }
+    fs.writeFileSync('deployments.json', JSON.stringify(deployments, null, 4));
+    console.log("Deployments saved to deployments.json");
+}
+
+
+deployAndSetup().then(() => process.exit(0)).catch(error => {
+    console.error(error);
+    process.exit(1);
+});
